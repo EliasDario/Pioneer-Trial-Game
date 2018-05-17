e83fe6d55409d191c77724cbe88ff431aa9fed7b
Isaias <Isaias@192.168.1.7>
2018-05-17 14:23:23 -0500

    This is the game class

---
+++ Pioneer-Trial-Game/src/byui/cit260/pioneer/trial/game/model/Game.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pioneer.trial.game.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Isaias
+ */
+public class Game implements Serializable{
+    // class instance variables
+    private double totalTime;
+    private int milesTraveled;
+
+    public Game() {
+    }
+        
+
+    public double getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(double totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public int getMilesTraveled() {
+        return milesTraveled;
+    }
+
+    public void setMilesTraveled(int milesTraveled) {
+        this.milesTraveled = milesTraveled;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.totalTime) ^ (Double.doubleToLongBits(this.totalTime) >>> 32));
+        hash = 47 * hash + this.milesTraveled;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (Double.doubleToLongBits(this.totalTime) != Double.doubleToLongBits(other.totalTime)) {
+            return false;
+        }
+        if (this.milesTraveled != other.milesTraveled) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "totalTime=" + totalTime + ", milesTraveled=" + milesTraveled + '}';
+    }
+    
+}
